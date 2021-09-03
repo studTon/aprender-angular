9b08cd2730509567f0bb55fb0560c4610b3704f6
studTon <evertonsdasilva1@gmail.com>
2021-09-02 22:12:52 -0300

    2 files modified, 1 added

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'meu-primeiro-component'
+  template:`<p>Meu Primeiro Component</p>`
+})
+export class MeuPrimeiroComponent {
+
+}
