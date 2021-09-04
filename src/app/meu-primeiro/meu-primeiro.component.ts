0cb4eaf59bef07d0302761cf8584216eae6f81aa
studTon <bra.17.ess@gmail.com>
2021-09-03 22:53:04 -0300

    Refiz os exercícios Estudei os até módulos

--- src/app/meu-primeiro/meu-primeiro.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'meu-primeiro-component',
-  template: `
-    <p>Meu Primeiro Component</p>
-  `
-})
-export class MeuPrimeiroComponent {}
