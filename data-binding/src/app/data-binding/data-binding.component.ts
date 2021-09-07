b4f168be290772e27cdd941c9d9c8412587883f1
studTon <evertonsdasilva1@gmail.com>
2021-09-06 22:11:03 -0300

    Editei até two way data binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -24,6 +24,7 @@ export class DataBindingComponent implements OnInit {
   valorAtual: string = '';
   valorSalvo: string = '';
   isMouseOver: boolean = false;
+  nome: string = 'abc';
   
   getValor() {
   	return 1;
