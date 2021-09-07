8cf66b02c920e7e7ae674be6299594bffe464d5a
studTon <evertonsdasilva1@gmail.com>
2021-09-07 14:51:42 -0300

    Edição finalizada do ciclo de vida

--- data-binding/src/app/ciclo/ciclo.component.ts
+++ data-binding/src/app/ciclo/ciclo.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+    Component, 
+    OnInit, 
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-ciclo',
   templateUrl: './ciclo.component.html',
   styleUrls: ['./ciclo.component.css']
 })
-export class CicloComponent implements OnInit {
-
-  constructor() { }
+export class CicloComponent implements OnInit, OnChanges, DoCheck, OnDestroy, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit {
+    @Input() valorInicial: number = 10;
+   
+   constructor() {
+    console.log('construtor');
+   }
+   
+   ngOnChanges(){
+     console.log('ngOnChanges');
+   }
 
   ngOnInit(): void {
+      console.log('ngOnInit');
   }
 
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
 }
