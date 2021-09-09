8614307ee63098babc591b81ef01adeeb65edee3
studTon <evertonsdasilva1@gmail.com>
2021-09-09 12:02:05 -0300

    Terminei a aula sobre ngswitch Terminei também a aula sobre ngif

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,14 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.scss']
+  styleUrls: ['./diretiva-ngif.component.css']
 })
 export class DiretivaNgifComponent implements OnInit {
+
+  cursos: any = [];
+  mostrarCursos: boolean = false;
 
   constructor() { }
 
   ngOnInit(): any{
     return 1;
+  }
+
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos;
   }
 
 }
