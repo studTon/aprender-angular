aac508714ada0c6d6a6bddb77ac56eb2f72fe80f
studTon <evertonsdasilva1@gmail.com>
2021-09-09 14:07:16 -0300

    Diretivas ngfor

--- diretivas/src/app/diretiva-ngfor/diretiva-ngfor.component.ts
+++ diretivas/src/app/diretiva-ngfor/diretiva-ngfor.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngfor',
   templateUrl: './diretiva-ngfor.component.html',
-  styleUrls: ['./diretiva-ngfor.component.scss']
+  styleUrls: ['./diretiva-ngfor.component.css']
 })
 export class DiretivaNgforComponent implements OnInit {
+  cursos: any = ["Angular", "Java", "JS"];
 
   constructor() { }
 
