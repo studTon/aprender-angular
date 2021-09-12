9d9e7127f8082210e1120797471ce6da22b6879e
studTon <evertonsdasilva1@gmail.com>
2021-09-12 09:59:26 -0300

    Injeção de dependência Também como usar um serviço em um componente

--- servicos/src/app/cursos/cursos.component.ts
+++ servicos/src/app/cursos/cursos.component.ts
@@ -8,13 +8,14 @@ import { CursosService } from './cursos.service';
 })
 export class CursosComponent implements OnInit {
   cursos: string[] = [];
-  cursosService: CursosService;
-  constructor() { 
-    this.cursosService = new CursosService();
+  //cursosService: CursosService;
+  constructor(private _cursosService: CursosService) { 
+    //this.cursosService = new CursosService();
+    //this.cursosService = _cursosService;
   }
 
   ngOnInit(): void {
-    this.cursos = this.cursosService.getCursos();
+    this.cursos = this._cursosService.getCursos();
   }
 
 }
