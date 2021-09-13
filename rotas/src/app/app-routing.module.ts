47c294a7d48a5d25b38fe3b326d4b55999c8d2da
studTon <evertonsdasilva1@gmail.com>
2021-09-13 14:55:13 -0300

    Criação de rota simples

---
+++ rotas/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'cursos', component: CursosComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'home', component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+
+export class AppRoutingModule { }
